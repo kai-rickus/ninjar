92a4b8f5de4843cb258c5e2a47426752ea76731b
Kai-Rickus <kai.rickus@outlook.com>
2023-11-04 10:21:50 +0100

    Remove logs Remove example image loading

--- game/src/Bootloader.ts
+++ game/src/Bootloader.ts
@@ -6,14 +6,10 @@ export class Bootloader extends Scene
         super( {
             key : 'Bootloader'
         } );
-        console.log( 'Scene Bootloader' );
     }
 
     preload(): void
     {
-        this.load.setPath( 'assets/' );
-        this.load.image( 'phaser3_cli' );
-
         this.load.on( 'complete', () =>
         {
             this.scene.start( 'MainScene' );
