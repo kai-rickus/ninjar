78a7986c6cd8d6ca50180391f06f65d3a618825f
kai-rickus <kai.rickus@outlook.com>
2023-11-07 18:25:09 +0100

    Refactor main scene

--- game/src/objects/ninjar.ts
+++ game/src/objects/ninjar.ts
@@ -1,6 +1,72 @@
-import { GameObjects } from 'phaser';
+import * as Phaser   from 'phaser';
+import { Types }     from 'phaser';
+import { MainScene } from '../scenes/MainScene';
 
-export class Ninjar extends GameObjects.GameObject
+export class Ninjar
 {
+    get spriteBody(): Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
+    {
+        return this._spriteBody;
+    }
 
+    private static readonly _RUN_KEY  = 'ninjar_run';
+    private static readonly _IDLE_KEY = 'ninjar_idle';
+    private static readonly _JUMP_KEY = 'ninjar_jump';
+
+    private _spriteBody: Types.Physics.Arcade.SpriteWithDynamicBody;
+
+    public constructor( x: number, y: number, scene: MainScene )
+    {
+        this._spriteBody = scene.physics.add.sprite( x, y, Ninjar._IDLE_KEY );
+        this._spriteBody.setBounce( 0 );
+        this._spriteBody.setCollideWorldBounds( true );
+
+        scene.anims.create( {
+            key       : 'left',
+            frames    : scene.anims.generateFrameNumbers( Ninjar._RUN_KEY, { start : 0, end : 9 } ),
+            repeat    : -1,
+            frameRate : 20
+        } );
+
+        scene.anims.create( {
+            key       : 'right',
+            frames    : scene.anims.generateFrameNumbers( Ninjar._RUN_KEY, { start : 0, end : 9 } ),
+            repeat    : -1,
+            frameRate : 20
+        } );
+        scene.anims.create( {
+            key       : 'idle',
+            frames    : scene.anims.generateFrameNumbers( Ninjar._IDLE_KEY, { start : 0, end : 9 } ),
+            repeat    : -1,
+            frameRate : 20
+        } );
+        scene.anims.create( {
+            key       : 'jump',
+            frames    : scene.anims.generateFrameNumbers( Ninjar._JUMP_KEY, { start : 0, end : 2 } ),
+            repeat    : 0,
+            frameRate : 20,
+        } );
+    }
+
+    public static loadSpriteSheets( scene: MainScene )
+    {
+        scene.load.spritesheet( Ninjar._RUN_KEY,
+            'assets/spritesheets/player/run.png',
+            {
+                frameWidth : 120, frameHeight : 80
+            }
+        );
+        scene.load.spritesheet( Ninjar._IDLE_KEY,
+            'assets/spritesheets/player/idle.png',
+            {
+                frameWidth : 120, frameHeight : 80
+            }
+        );
+        scene.load.spritesheet( Ninjar._JUMP_KEY,
+            'assets/spritesheets/player/jump.png',
+            {
+                frameWidth : 120, frameHeight : 80
+            }
+        );
+    }
 }
--- game/src/scenes/MainScene.ts
+++ game/src/scenes/MainScene.ts
@@ -1,20 +1,16 @@
-import { Scene, GameObjects } from 'phaser';
-import { Ninjar }             from '../objects/ninjar';
+import { Scene, Types, Physics } from 'phaser';
+import { Ninjar }                from '../objects/ninjar';
 
 export class MainScene extends Scene
 {
-    public image: GameObjects.Image;
-    public ninjar: Ninjar;
+    private static readonly _SKYBG_KEY       = 'sky';
+    private static readonly _MOUNTAINBG_KEY  = 'mountain';
+    private static readonly _GRASSLANDBG_KEY = 'grassland';
+    private static readonly _GRASSTILE_KEY   = 'grasstile';
 
-    private SKYBG_KEY       = 'sky';
-    private MOUNTAINBG_KEY  = 'mountain';
-    private GRASSLANDBG_KEY = 'grassland';
-    private GRASSTILE_KEY   = 'grasstile';
-    private PLAYER_KEY      = 'ninjar';
-    private player;
-    private playerRun;
-    private cursors;
-    private groundPlatform;
+    private _player: Ninjar;
+    private _cursors: Types.Input.Keyboard.CursorKeys;
+    private _groundPlatforms: Physics.Arcade.StaticGroup;
 
     constructor()
     {
@@ -25,56 +21,37 @@ export class MainScene extends Scene
 
     preload()
     {
-        this.load.image( this.SKYBG_KEY, 'assets/images/sky.png' );
-        this.load.image( this.MOUNTAINBG_KEY, 'assets/images/mountain.png' );
-        this.load.image( this.GRASSLANDBG_KEY, 'assets/images/grassland.png' );
-        this.load.image( this.GRASSLANDBG_KEY, 'assets/images/grassland.png' );
+        this.load.image( MainScene._SKYBG_KEY, 'assets/images/sky.png' );
+        this.load.image( MainScene._MOUNTAINBG_KEY, 'assets/images/mountain.png' );
+        this.load.image( MainScene._GRASSLANDBG_KEY, 'assets/images/grassland.png' );
 
-        this.load.image( this.GRASSTILE_KEY, 'assets/tiles/green_34.png' );
+        this.load.image( MainScene._GRASSTILE_KEY, 'assets/tiles/green_34.png' );
 
-        // this.player = this.load.spritesheet( this.PLAYER_KEY,
-        //     'assets/spritesheets/player/run.png',
-        //     {
-        //         frameWidth : 120, frameHeight : 80
-        //     }
-        // );
-
-        this.player = this.load.spritesheet( this.PLAYER_KEY,
-            'assets/spritesheets/player/idle.png',
-            {
-                frameWidth : 120, frameHeight : 80
-            }
-        );
-
-        this.playerRun = this.load.spritesheet( this.PLAYER_KEY,
-            'assets/spritesheets/player/run.png',
-            {
-                frameWidth : 120, frameHeight : 80
-            }
-        );
+        Ninjar.loadSpriteSheets( this );
     }
 
     create(): void
     {
+        this.createBackground();
 
-        const sky = this.add.image( this.scale.width / 2, this.scale.height / 2, this.SKYBG_KEY );
-        sky.setScale( 4 );
-        const mountain = this.add.image( this.scale.width / 2, this.scale.height / 2, this.MOUNTAINBG_KEY );
-        mountain.setScale( 4 );
-        const grassland = this.add.image( this.scale.width / 2, this.scale.height / 2, this.GRASSLANDBG_KEY );
-        grassland.setScale( 4 );
+        this._player = new Ninjar( 400, 520, this );
 
-        // const grasstile = this.add.image( this.scale.width / 2, this.scale.height / 2, this.grasstile );
-
-        // this.image = this.add.image( this.scale.width / 2, this.scale.height / 2, 'phaser3_cli' );
-
-        this.player = this.createPlayer();
         const platforms = this.createPlatforms();
 
-        this.physics.add.collider( this.player, platforms );
-        this.physics.add.collider( this.player, this.groundPlatform );
+        this.physics.add.collider( this._player.spriteBody, platforms );
+        this.physics.add.collider( this._player.spriteBody, this._groundPlatforms );
 
-        this.cursors = this.input.keyboard.createCursorKeys();
+        this._cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    createBackground()
+    {
+        const sky = this.add.image( this.scale.width / 2, this.scale.height / 2, MainScene._SKYBG_KEY );
+        sky.setScale( 4 );
+        const mountain = this.add.image( this.scale.width / 2, this.scale.height / 2, MainScene._MOUNTAINBG_KEY );
+        mountain.setScale( 4 );
+        const grassland = this.add.image( this.scale.width / 2, this.scale.height / 2, MainScene._GRASSLANDBG_KEY );
+        grassland.setScale( 4 );
 
     }
 
@@ -82,84 +59,45 @@ export class MainScene extends Scene
     {
         const platforms = this.physics.add.staticGroup();
 
-        // platforms.create( 400, 568, this.GRASSTILE_KEY ).setScale( 4 ).refreshBody();
-        //
-        // platforms.create( 600, 400, this.GRASSTILE_KEY );
-        // platforms.create( 50, 250, this.GRASSTILE_KEY );
-        // platforms.create( 750, 220, this.GRASSTILE_KEY );
-
-
-        this.groundPlatform = this.physics.add.staticGroup({
-            key: this.GRASSTILE_KEY,
-            repeat: 150,
-            setXY: { x: 0, y: 568, stepX: 15 }
-        })
-
-
-        // groundPlatform.children.iterate((child) => {
-        //     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
-        // })
+        this._groundPlatforms = this.physics.add.staticGroup( {
+            key    : MainScene._GRASSTILE_KEY,
+            repeat : 150,
+            setXY  : { x : 0, y : 568, stepX : 15 }
+        } );
 
         return platforms;
     }
 
-    createPlayer()
-    {
-        this.playerRun = this.physics.add.sprite( 400, 450, this.PLAYER_KEY );
-        this.playerRun.setBounce( 0.2 );
-        this.playerRun.setCollideWorldBounds( true );
-
-        this.anims.create( {
-            key       : 'left',
-            frameRate : 10,
-            repeat    : -1
-        } );
-
-        // this.anims.create( {
-        //     key       : 'turn',
-        //     frames    : [ { key : this.PLAYER_KEY, frame : 4 } ],
-        //     frameRate : 20
-        // } );
-
-        this.anims.create( {
-            key       : 'right',
-            frameRate : 10,
-            repeat    : -1
-        } );
-
-        this.anims.create( {
-            key       : 'up',
-            frameRate : 10,
-            repeat    : -1
-        } );
-
-        return this.playerRun;
-    }
-
     update()
     {
-        if ( this.cursors.left.isDown )
+        if ( this._cursors.right.isDown )
         {
-            this.playerRun.setVelocityX( -320 );
+            this._player.spriteBody.setVelocityX( 320 );
+            this._player.spriteBody.setFlipX( false );
 
-            this.playerRun.anims.play( 'left', true );
-        }else if ( this.cursors.right.isDown )
+            this._player.spriteBody.anims.play( 'right', true );
+        }else if ( this._cursors.left.isDown )
         {
-            this.playerRun.setVelocityX( 320 );
+            this._player.spriteBody.setVelocityX( -320 );
+            this._player.spriteBody.setFlipX( true );
 
-            this.playerRun.anims.play( 'right', true );
-        }
-        else
+            this._player.spriteBody.anims.play( 'left', true );
+        }else
         {
-            this.player.setVelocityX(0)
-
-            this.player.anims.play('turn')
-        }
-
-        if ( this.cursors.up.isDown && this.playerRun.body.touching.down )
-        {
-            this.playerRun.setVelocityY( 760 );
+            this._player.spriteBody.setVelocityX( 0 );
+            this._player.spriteBody.anims.play( 'idle' );
         }
 
+        if ( this._cursors.up.isDown && this._player.spriteBody.body.touching.down )
+        {
+            this._player.spriteBody.setVelocityY( -200 );
+            this._player.spriteBody.anims.stop();
+            this._player.spriteBody.anims.play( 'jump' );
+        }
+
+        if ( !this._player.spriteBody.body.touching.down )
+        {
+            this._player.spriteBody.anims.play( 'jump' );
+        }
     }
 }
