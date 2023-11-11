cf0d54a6b2300a4282f886765e82f8aeebf5afaa
Kai-Rickus <kai.rickus@outlook.com>
2023-11-11 18:38:08 +0100

    Add more spritesheets

--- game/src/objects/ninjar.ts
+++ game/src/objects/ninjar.ts
@@ -12,6 +12,8 @@ export class Ninjar
     private static readonly _RUN_KEY  = 'ninjar_run';
     private static readonly _IDLE_KEY = 'ninjar_idle';
     private static readonly _JUMP_KEY = 'ninjar_jump';
+    private static readonly _FALL_KEY = 'ninjar_fall';
+    private static readonly _DASH_KEY = 'ninjar_dash';
 
     private _spriteBody: Types.Physics.Arcade.SpriteWithDynamicBody;
 
@@ -46,6 +48,18 @@ export class Ninjar
             repeat    : 0,
             frameRate : 20,
         } );
+        scene.anims.create( {
+            key       : 'fall',
+            frames    : scene.anims.generateFrameNumbers( Ninjar._FALL_KEY, { start : 0, end : 2 } ),
+            repeat    : 0,
+            frameRate : 20,
+        } );
+        scene.anims.create( {
+            key       : 'dash',
+            frames    : scene.anims.generateFrameNumbers( Ninjar._DASH_KEY, { start : 0, end : 1 } ),
+            repeat    : 0,
+            frameRate : 20,
+        } );
     }
 
     public static loadSpriteSheets( scene: MainScene )
@@ -64,6 +78,18 @@ export class Ninjar
         );
         scene.load.spritesheet( Ninjar._JUMP_KEY,
             'assets/spritesheets/player/jump.png',
+            {
+                frameWidth : 120, frameHeight : 80
+            }
+        );
+        scene.load.spritesheet( Ninjar._FALL_KEY,
+            'assets/spritesheets/player/fall.png',
+            {
+                frameWidth : 120, frameHeight : 80
+            }
+        );
+        scene.load.spritesheet( Ninjar._DASH_KEY,
+            'assets/spritesheets/player/dash.png',
             {
                 frameWidth : 120, frameHeight : 80
             }
