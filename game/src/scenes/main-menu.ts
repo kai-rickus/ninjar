78208bc30fc112bda780741556589abdebe5b301
KaiRi <kai.rickus@outlook.com>
2024-03-16 18:09:14 +0100

    Add menu audio
    Refactor hardcoded values into variables
    
    Change keyboard controls

--- game/src/scenes/main-menu.ts
+++ game/src/scenes/main-menu.ts
@@ -1,59 +1,66 @@
-import { SceneOne }       from "./scene-one"
-import { Scene, Physics } from "phaser"
+import { WASD_CONTROLS, WASDControls } from "../objects/controls"
+import { SceneOne }                    from "./scene-one"
+import { Scene, Physics }              from "phaser"
 
 export class MainMenu extends Scene
 {
-    private cursors: Phaser.Types.Input.Keyboard.CursorKeys
-    private buttons: Phaser.GameObjects.Image[] = []
-    private selectedButtonIndex                 = 0
-    private buttonSelector!: Phaser.GameObjects.Image
+    private _buttons: Phaser.GameObjects.Image[] = []
+    private _selectedButtonIndex                 = 0
+    private _buttonSelectorCursor!: Phaser.GameObjects.Image
 
-    constructor()
+    constructor( private _controls: WASDControls, private _scene: Scene )
     {
         super( "main-menu" )
-    }
-
-    init()
-    {
-        this.cursors = this.input.keyboard.createCursorKeys()
     }
 
     preload()
     {
         this.load.image( "glass-panel", "assets/buttons/scroll-button.png" )
         this.load.image( "cursor-hand", "assets/cursors/menu-cursor.png" )
+
+        this._controls = this.input.keyboard.addKeys( WASD_CONTROLS ) as WASDControls
+        this.load.audio('menu-change', 'assets/audio/menu/menu-change.mp3')
+        this.load.audio('menu-select', 'assets/audio/menu/menu-select.mp3')
+
     }
 
     create()
     {
-        const { width, height } = this.scale
+        const buttonWidth          = 200
+        const buttonHeight         = 75
+        const centeredTextValue    = 0.5
+        const centeredButtonWidth  = 0.5
+        const centeredButtonHeight = 0.6
+        const cursorScale          = 0.4
+        const buttonGap            = 10
+        const playButtonText       = "Spielen"
+        const settingsButtonText   = "Einstellungen"
+        const creditsButtonText    = "Credits"
+        const { width, height }    = this.scale
 
-        // Play button
-        const playButton = this.add.image( width * 0.5, height * 0.6, "glass-panel" )
-                               .setDisplaySize( 150, 50 )
+        const playButton = this.add.image( width * centeredButtonWidth, height * centeredButtonHeight, "glass-panel" )
+                               .setDisplaySize( buttonWidth, buttonHeight )
 
-        this.add.text( playButton.x, playButton.y, "Play" )
-            .setOrigin( 0.5 )
+        this.add.text( playButton.x, playButton.y, playButtonText )
+            .setOrigin( centeredTextValue )
 
-        // Settings button
-        const settingsButton = this.add.image( playButton.x, playButton.y + playButton.displayHeight + 10, "glass-panel" )
-                                   .setDisplaySize( 150, 50 )
+        const settingsButton = this.add.image( playButton.x, playButton.y + playButton.displayHeight + buttonGap, "glass-panel" )
+                                   .setDisplaySize( buttonWidth, buttonHeight )
 
-        this.add.text( settingsButton.x, settingsButton.y, "Settings" )
-            .setOrigin( 0.5 )
+        this.add.text( settingsButton.x, settingsButton.y, settingsButtonText )
+            .setOrigin( centeredTextValue )
 
-        // Credits button
-        const creditsButton = this.add.image( settingsButton.x, settingsButton.y + settingsButton.displayHeight + 10, "glass-panel" )
-                                  .setDisplaySize( 150, 50 )
+        const creditsButton = this.add.image( settingsButton.x, settingsButton.y + settingsButton.displayHeight + buttonGap, "glass-panel" )
+                                  .setDisplaySize( buttonWidth, buttonHeight )
 
-        this.add.text( creditsButton.x, creditsButton.y, "Credits" )
-            .setOrigin( 0.5 )
+        this.add.text( creditsButton.x, creditsButton.y, creditsButtonText )
+            .setOrigin( centeredTextValue )
 
-        this.buttons.push( playButton )
-        this.buttons.push( settingsButton )
-        this.buttons.push( creditsButton )
+        this._buttons.push( playButton )
+        this._buttons.push( settingsButton )
+        this._buttons.push( creditsButton )
 
-        this.buttonSelector = this.add.image( 0, 0, "cursor-hand" ).setScale( 0.4 )
+        this._buttonSelectorCursor = this.add.image( 0, 0, "cursor-hand" ).setScale( cursorScale )
 
         this.selectButton( 0 )
 
@@ -65,86 +72,99 @@ export class MainMenu extends Scene
 
         settingsButton.on( "selected", () =>
         {
+            // todo: settings implementieren
             console.log( "settings" )
         } )
 
         creditsButton.on( "selected", () =>
         {
+            // todo: credits implementieren
             console.log( "credits" )
         } )
     }
 
     startLevelOne()
     {
-        // this.scene.start( "SceneOne" )
-
-        this.scene.start('SceneOne')
+        this.scene.start( "SceneOne" )
     }
 
     selectButton( index: number )
     {
-        const currentButton = this.buttons[ this.selectedButtonIndex ]
+        const currentButtonTint = 0xffffff
+        const buttonTint        = 0x05bfed
+        const currentButton     = this._buttons[ this._selectedButtonIndex ]
 
-        // set the current selected button to a white tint
-        currentButton.setTint( 0xffffff )
+        currentButton.setTint( currentButtonTint )
 
-        const button = this.buttons[ index ]
+        const button = this._buttons[ index ]
 
-        // set the newly selected button to a green tint
-        button.setTint( 0x66ff7f )
+        button.setTint( buttonTint )
 
-        // move the hand cursor to the right edge
-        this.buttonSelector.x = button.x + button.displayWidth * 0.5
-        this.buttonSelector.y = button.y + 10
+        this._buttonSelectorCursor.x = button.x + button.displayWidth * 0.5
+        this._buttonSelectorCursor.y = button.y + 10
 
-        // store the new selected index
-        this.selectedButtonIndex = index
+        this._selectedButtonIndex = index
 
     }
 
     selectNextButton( change = 1 )
     {
-        let index = this.selectedButtonIndex + change
+        let index = this._selectedButtonIndex + change
+        this.handleAudioCues( "menu-change" )
 
-        // wrap the index to the front or end of array
-        if ( index >= this.buttons.length )
+        if ( index >= this._buttons.length )
         {
             index = 0
         }
         else if ( index < 0 )
         {
-            index = this.buttons.length - 1
+            index = this._buttons.length - 1
         }
-
         this.selectButton( index )
-
     }
 
     confirmSelection()
     {
-        // get the currently selected button
-        const button = this.buttons[ this.selectedButtonIndex ]
+        this.handleAudioCues( "menu-select" )
+        const button = this._buttons[ this._selectedButtonIndex ]
 
-        // emit the 'selected' event
         button.emit( "selected" )
+    }
+
+    handleAudioCues(  Cue: string )
+    {
+        const menuChange = this.sound.add( "menu-change" )
+        const menuSelect = this.sound.add( "menu-select" )
+
+        if ( Cue === "menu-change" )
+        {
+            menuChange.setVolume( 0.01 )
+            menuChange.play()
+        }
+        else if ( Cue === "menu-select" )
+        {
+            menuSelect.setVolume( 1 )
+            menuSelect.play()
+        }
     }
 
     update()
     {
-        const upJustPressed    = Phaser.Input.Keyboard.JustDown( this.cursors.up )
-        const downJustPressed  = Phaser.Input.Keyboard.JustDown( this.cursors.down )
-        const spaceJustPressed = Phaser.Input.Keyboard.JustDown( this.cursors.space )
+        const { w, s, space } = this._controls
 
-        if ( upJustPressed )
+        if ( w.isDown )
         {
+            w.reset()
             this.selectNextButton( -1 )
         }
-        else if ( downJustPressed )
+        else if ( s.isDown )
         {
+            s.reset()
             this.selectNextButton( 1 )
         }
-        else if ( spaceJustPressed )
+        else if ( space.isDown )
         {
+            space.reset()
             this.confirmSelection()
         }
 
